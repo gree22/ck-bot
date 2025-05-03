ddd8ffc2925d2361fa97bab52ef93454cc371a7b
gree22 <gregorydelossantos77@gmail.com>
2025-05-03 11:13:47 -1000

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
-BOT_TOKEN = "7945172050:AAFMFS_N1k3ziraeHHd0r88lrR5Dzeo6Hmw"
-WEBAPP_URL = "https://gree22.github.io/mi-flask-app/"
+BOT_TOKEN = "tu_token_aqui"
+WEBAPP_URL = "https://gree22.github.io/mi-flask-app"
+
